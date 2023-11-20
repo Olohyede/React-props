ccf9b71286c4e36306c989d9df22bc8f74bb9559
Olohyede <oloyedet70@gmail.com>
2023-11-20 03:02:51 +0100

    final commit

---
+++ src/player.js
@@ -0,0 +1,57 @@
+import Pedri from './images/Pedri.jpeg'
+import Haaland from './images/Haaland.jpeg'
+import Musiala from './images/Musiala.jpeg'
+import Camavinga from './images/Camavinga.jpeg'
+import Alvarez from './images/Alvarez.jpeg'
+
+
+// Attributes for each players
+
+const player = [
+    {
+        name: "Pedri Gonzalez",
+        team: "Barcelona",
+        nationality: "Spain",
+        jerseyNumber: 8,
+        age: 20,
+        image: Pedri,
+    },
+
+    {
+        name: "Erling Haaland",
+        team: "Manchester City",
+        nationality: "Norway",
+        jerseyNumber: 9,
+        age: 23,
+        image: Haaland,
+    },
+
+    {
+        name: "Jamal Musiala",
+        team: "Bayern Munich",
+        nationality: "Germany",
+        jerseyNumber: 42,
+        age: 20,
+        image: Musiala,
+    },
+
+    {
+        name: "Eduardo Camavinga",
+        team: "Real Madrid",
+        nationality: "France",
+        jerseyNumber: 12,
+        age: 21,
+        image: Camavinga,
+    },
+
+    {
+        name: "Julian Alvarez",
+        team: "Manchester City",
+        nationality: "Argentina",
+        jerseyNumber: 19,
+        age: 23,
+        image: Alvarez,
+    },
+];
+
+export default player;
